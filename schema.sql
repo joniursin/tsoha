ec4b68ecc52480b83e5d698ff9dbbb5d4956adcf
joni ursin <joni.ursin@gmail.com>
2023-04-02 05:09:57 +0300

    Added SQL schema

---
+++ schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE restaurants (id SERIAL PRIMARY KEY, name TEXT);
+INSERT INTO restaurants (name) VALUES ('Dilber');
+INSERT INTO restaurants (name) VALUES ('King Kebab');
+INSERT INTO restaurants (name) VALUES ('Hesburger');
