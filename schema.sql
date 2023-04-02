3e3c4ea71cca28956ee15348c6fe56e2f9e93f94
joni ursin <joni.ursin@gmail.com>
2023-04-02 08:05:54 +0300

    Updated tables

--- schema.sql
+++ schema.sql
@@ -1,4 +1,10 @@
+DROP TABLE restaurants CASCADE;
+DROP TABLE users CASCADE;
+DROP TABLE reviews CASCADE;
 CREATE TABLE restaurants (id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, op_status BOOLEAN);
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, user_id INTEGER REFERENCES users, rating INT, content TEXT, created_at TIMESTAMP);
 INSERT INTO restaurants (name) VALUES ('Dilber');
 INSERT INTO restaurants (name) VALUES ('King Kebab');
 INSERT INTO restaurants (name) VALUES ('Hesburger');
+INSERT INTO users (name, op_status) VALUES ('Kalle', FALSE);
