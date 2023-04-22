783c76b54d312faa66f8e9f944e635f091620ebc
joni ursin <joni.ursin@gmail.com>
2023-04-23 01:03:34 +0300

    added if exists

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE restaurants CASCADE;
-DROP TABLE users CASCADE;
-DROP TABLE reviews CASCADE;
+DROP TABLE IF EXISTS restaurants CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
 CREATE TABLE restaurants (id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, op_status BOOLEAN);
 CREATE TABLE reviews (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, user_id INTEGER REFERENCES users, username TEXT, rating INT, content TEXT, created_at TIMESTAMP, visible BOOLEAN);
