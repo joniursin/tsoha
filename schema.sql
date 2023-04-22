b2216508c707faaab0c3f5fc39bdf9053887be2c
joni ursin <joni.ursin@gmail.com>
2023-04-23 00:56:58 +0300

    added visible to reviews

--- schema.sql
+++ schema.sql
@@ -1,6 +1,9 @@
+DROP TABLE restaurants CASCADE;
+DROP TABLE users CASCADE;
+DROP TABLE reviews CASCADE;
 CREATE TABLE restaurants (id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, op_status BOOLEAN);
-CREATE TABLE reviews (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, user_id INTEGER REFERENCES users, username TEXT, rating INT, content TEXT, created_at TIMESTAMP);
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, user_id INTEGER REFERENCES users, username TEXT, rating INT, content TEXT, created_at TIMESTAMP, visible BOOLEAN);
 INSERT INTO restaurants (name) VALUES ('Unicafe');
 INSERT INTO restaurants (name) VALUES ('King Kebab');
 INSERT INTO restaurants (name) VALUES ('Hesburger');
