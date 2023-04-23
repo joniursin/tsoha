890835d2266db00b70a27a0de27c3720c1eb35bf
joni ursin <joni.ursin@gmail.com>
2023-04-23 15:39:31 +0300

    added table likes

--- schema.sql
+++ schema.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS reviews CASCADE;
 CREATE TABLE restaurants (id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, op_status BOOLEAN);
 CREATE TABLE reviews (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, user_id INTEGER REFERENCES users, username TEXT, rating INT, content TEXT, created_at TIMESTAMP, visible BOOLEAN);
+CREATE TABLE likes (id SERIAL PRIMARY KEY, review_id INTEGER REFERENCES reviews, user_id INTEGER REFERENCES users);
 INSERT INTO restaurants (name) VALUES ('Unicafe');
 INSERT INTO restaurants (name) VALUES ('King Kebab');
 INSERT INTO restaurants (name) VALUES ('Hesburger');
