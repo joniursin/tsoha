4f0f3b1e2fddfe7a101cf51b9f070ce726633317
joni ursin <joni.ursin@gmail.com>
2023-05-07 01:02:30 +0300

    added right page redirect when leaving or removing likes

--- like.py
+++ like.py
@@ -21,18 +21,30 @@ def get_likes(review_id):
 def like():
     review_id = request.form["review_id"]
     user_id = request.form["user_id"]
+    restaurant_name = request.form["restaurant_name"]
 
     sql = "INSERT INTO likes (review_id, user_id) VALUES (:review_id, :user_id)"
     db.session.execute(text(sql), {"review_id":review_id, "user_id":user_id})
     db.session.commit()
-    return redirect("/")
+
+    sql = "SELECT id FROM restaurants WHERE name=:restaurant_name"
+    result = db.session.execute(text(sql), {"restaurant_name":restaurant_name})
+    restaurant_id = result.fetchone()[0]
+
+    return redirect("/view/" + str(restaurant_id))
 
 @app.route("/remove_like", methods=["POST"])
 def remove_like():
     review_id = request.form["review_id"]
     user_id = request.form["user_id"]
+    restaurant_name = request.form["restaurant_name"]
 
     sql = "DELETE FROM likes WHERE review_id=:review_id AND user_id=:user_id"
     db.session.execute(text(sql), {"review_id":review_id, "user_id":user_id})
     db.session.commit()
-    return redirect("/")
+
+    sql = "SELECT id FROM restaurants WHERE name=:restaurant_name"
+    result = db.session.execute(text(sql), {"restaurant_name":restaurant_name})
+    restaurant_id = result.fetchone()[0]
+
+    return redirect("/view/" + str(restaurant_id))
