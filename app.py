a706136fbfad1e2170fac1ce469ffc8d9efeae59
joni ursin <joni.ursin@gmail.com>
2023-05-06 22:56:34 +0300

    removed useless imports

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
-from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
