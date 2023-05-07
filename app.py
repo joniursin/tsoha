40abfae9f67a172877dc2360e23db423277c310b
joni ursin <joni.ursin@gmail.com>
2023-05-07 17:22:55 +0300

    moved group creation to group.py

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ def admin_status():
         return True
     return False
 
+import group
+
 @app.route("/")
 def index():
     sql = "SELECT id, name FROM restaurants"
@@ -97,30 +99,4 @@ def add_restaurant():
     db.session.commit()
     return redirect("/")
 
-@app.route("/new_group")
-def new_group():
-    sql = "SELECT id, name FROM restaurants"
-    result = db.session.execute(text(sql))
-    restaurants = result.fetchall()
-    return render_template("new-group.html", admin_status=admin_status, restaurants=restaurants)
-
-@app.route("/add_group", methods=["POST"])
-def add_group():
-    if session["csrf_token"] != request.form["csrf_token"]:
-        abort(403)
-    group_name = request.form["name"]
-    if group_name == "":
-        return render_template("error.html", error="Nimeä ryhmälle ei annettu!")
-    
-    chosen_restaurants = request.form.getlist("restaurant_id")
-
-    sql = "INSERT INTO groups (group_name) VALUES (:group_name) RETURNING id"
-    group_id = db.session.execute(text(sql), {"group_name":group_name}).fetchone()[0]
-
-    for i in chosen_restaurants:
-        sql = "INSERT INTO group_members (group_id, restaurant_id) VALUES (:group_id, :restaurant_id)"
-        db.session.execute(text(sql), {"group_id":group_id, "restaurant_id":int(i)})
-
-    db.session.commit()
-    return redirect("/")
 
