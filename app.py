d8886a6d80eec3d32401b1bacf08b745f1ee1ba5
joni ursin <joni.ursin@gmail.com>
2023-05-07 03:29:23 +0300

    added ability to make groups

--- app.py
+++ app.py
@@ -90,4 +90,29 @@ def add_restaurant():
     db.session.commit()
     return redirect("/")
 
+@app.route("/new_group")
+def new_group():
+    sql = "SELECT id, name FROM restaurants"
+    result = db.session.execute(text(sql))
+    restaurants = result.fetchall()
+    return render_template("new-group.html", admin_status=admin_status, restaurants=restaurants)
+
+@app.route("/add_group", methods=["POST"])
+def add_group():
+    try:
+        group_name = request.form["name"]
+    except:
+        return render_template("error.html", error="Nimeä ryhmälle ei annettu!")
+    
+    chosen_restaurants = request.form.getlist("restaurant_id")
+
+    sql = "INSERT INTO groups (group_name) VALUES (:group_name) RETURNING id"
+    group_id = db.session.execute(text(sql), {"group_name":group_name}).fetchone()[0]
+
+    for i in chosen_restaurants:
+        sql = "INSERT INTO group_members (group_id, restaurant_id) VALUES (:group_id, :restaurant_id)"
+        db.session.execute(text(sql), {"group_id":group_id, "restaurant_id":int(i)})
+
+    db.session.commit()
+    return redirect("/")
 
